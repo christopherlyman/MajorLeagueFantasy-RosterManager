533774d639f19ff78d83a127a9eecd9dffa6cde7
Christopher Lyman <chris.h.lyman@gmail.com>
2026-05-17 16:34:57 -0400

    Move Streamlit routed pages out of reserved pages directory

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-batters = st.Page("pages/batters.py", title="Batters", default=True)
-pitchers = st.Page("pages/pitchers.py", title="Pitchers")
+batters = st.Page("views/batters.py", title="Batters", default=True)
+pitchers = st.Page("views/pitchers.py", title="Pitchers")
 
 pg = st.navigation([batters, pitchers])
 pg.run()
